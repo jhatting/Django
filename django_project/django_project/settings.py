f2a3b026ea29de1fbd75415c07339d644a0a6ab8
Jarrod Hatting <jarrodhatting@icloud.com>
2020-08-17 12:46:23 -0700

    update

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -126,3 +126,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'djangotest1985@gmail.com'
+EMAIL_HOST_PASSWORD = 'uemxjtbfpinhzrxj'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'default from email'
